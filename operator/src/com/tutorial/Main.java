4aa83574408f086fa72efd6db7d83dcb2275bfa9
restuadipradana <restu.maduki@live.com>
2019-11-10 13:00:05 +0700

    update biar lengkap

---
+++ operator/src/com/tutorial/Main.java
@@ -0,0 +1,33 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //operator assignment jumlah
+        int a = 1;
+        a += 10;
+        System.out.println("Nilai a = " + a);
+
+        //operator assignment kurang
+        int b = 100;
+        b -= 25;
+        System.out.println("Nilai b = " + b);
+
+        //operator assignment kali
+        int c = 10;
+        c *= 10;
+        System.out.println("Nilai c = " + c);
+
+        //operator assignment bagi
+        int d = 100;
+        d /= 25;
+        System.out.println("Nilai d = " + d);
+
+        //operator assignment modulus
+        int e = 10;
+        e %= 7;
+        System.out.println("Nilai e = " + e);
+
+    }
+
+}
